b0a8acb1b01d9c8becc009ab84f42c9536eaaaee
Dempsey FP <dempseyfinley@gmail.com>
2020-10-07 21:29:00 -0400

    javascript updated, localStorage working

--- README.md
+++ README.md
@@ -1,21 +1,10 @@
 # 05 Third-Party APIs: Work Day Scheduler
 
-## Your Task
+# Overview
 
-Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-
-You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
-
-
-## User Story
-
-```
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
-
-
+* I was able to dynamically create the time blocks and buttons, but I got extremely stuck on the input and localStorage aspect of the page.
+ * I did have some trouble with the display date at first, as I was setting it as a variable. In doing so it would pull only the date and time of when the page was first loaded, and would not update in real-time. 
+ 
 ## Acceptance Criteria
 
 ```
@@ -35,61 +24,4 @@ THEN the saved events persist
 ```
 
 
-## Mock-Up
-
-The following animation demonstrates the application functionality:
-
-![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
-
-
-## Grading Requirements
-
-This homework is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-  * Uses the Moment.js library to work with date and time
-
-### Deployment: 32%
-
-* Application deployed at live URL
-
-* Application loads with no errors
-
-* Application GitHub URL submitted
-
-* GitHub repo contains application code
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate
-
-* Application user interface style is clean and polished
-
-* Application resembles the mock-up functionality provided in the homework instructions
-
-### Repository Quality: 13%
-
-* Repository has a unique name
-
-* Repository follows best practices for file structure and naming conventions
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages
-
-* Repository contains quality README file with description, screenshot, and link to deployed application
-
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2020 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+ 
--- script.js
+++ script.js
@@ -1,22 +1,72 @@
-var displayDate = document.getElementById("currentDay");
-// console.log(moment()._locale._calendar.lastDay);
-// console.log(document);
-// console.log(moment());
-// displayDate.innerHTML="Today's date: " + today;
-var timeArray = ["9 AM","10 AM","11 AM","12 PM","1 PM","2 PM","3 PM","4 PM","5 PM"];
-function resetDate(){
-    displayDate.innerHTML="Today's date: " + moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-}
-setInterval(resetDate, 1000);
-
-for(let i=0;i<9;i++){
-    var container=$(".container");
-    container.append(`<section class="row">
-    <time class="time-block col-md-2 col-sm-2">${timeArray[i]}</time>
-    <input class="description col-md-8 col-sm-8"></input>
-    <button class="saveBtn col-md-2 col-sm-2">click</button>
-    </section>
+$(document).ready(() => {
+    var displayDate = document.getElementById("currentDay");
+    // console.log(moment()._locale._calendar.lastDay);
+    // console.log(document);
+    // console.log(moment());
+    // displayDate.innerHTML="Today's date: " + today;
+    var timeArray = ["9 AM","10 AM","11 AM","12 PM","1 PM","2 PM","3 PM","4 PM","5 PM"];
+    function resetDate(){
+        displayDate.innerHTML="Today's date: " + moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    }
+    setInterval(resetDate, 1000);
     
-    `)
-}
+    var container=$(".container");
+    
+    function init() {
+        var tmpOBJ = window.localStorage.getItem("planner")
+        //not sure if this is needed but it erases the saved data after a day
+        if (!!tmpOBJ) {
+            tmpOBJ = JSON.parse(tmpOBJ)
+            if (tmpOBJ.expires > timeOBJ.expires) {
+                window.localStorage.removeItem("planner");
+            }else {
+                timeOBJ = tmpOBJ;
+            }
+        
+        }
+        for(var i=0;i<9;i++){
+            container.append(`<section class="row" id="${i}">
+            <time class="time-block col-md-2 col-sm-2">${timeArray[i]}</time>
+            <input class="description col-md-8 col-sm-8" Value="${timeOBJ[i.toString()]}"></input>
+            <button class="saveBtn col-md-2 col-sm-2">click</button>
+            </section>
+            
+            `)
+        }
+    }
+    
+    var timeOBJ = {
+        '0': '',
+        '1': '',
+        '2': '',
+        '3': '',
+        '4': '',
+        '5': '',
+        '6': '',
+        '7': '',
+        '8': '',
+        '9': '',
+        'expires': (new Date().getTime() + 86400000)
+    }
+    //use var if you need to stick with ecma 5
+    //ecma 6 uses let and const. 
+    
+    $(document).on('click','.saveBtn',function(){
+        console.log(this)
+        var parentEl = $(this).parent(),
+            textBEl = parentEl.find("input");
+        if (!!textBEl.length) {
+            var text = textBEl[0].value,
+                parentID = parentEl.attr("id");
+                if (!!parentID) {
+                    timeOBJ[parentID.toString()] = text
+                    window.localStorage.removeItem("planner");
+                    window.localStorage.setItem('planner', JSON.stringify(timeOBJ));
+                }
+        }
+        //window.localStorage.setItem('planner', JSON.stringify(timeOBJ));
+    });
+    window.onload = init();
+    })
+    
 
